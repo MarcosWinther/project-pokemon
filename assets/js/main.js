397c01ed9772086b7bf60d7b1d74a3336ba824b3
MarcosWinther <winthermarcos@gmail.com>
2022-11-29 23:32:16 -0300

    Separando o consumo da API da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,4 @@
-const offset = 0;
-const limit = 10;
-const url = `https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`;
+
 
 function convertPokemonToLi(pokemon) {
    return `
@@ -22,15 +20,14 @@ function convertPokemonToLi(pokemon) {
 
 const pokemonList = document.getElementById('pokemonList')
 
-fetch(url)
-   .then((response) => response.json())
-   .then((jsonBody) => jsonBody.results)
-   .then((pokemons) => {
-      
+pokeApi.getPokemons().then((pokemons) => {
+   const listItems = [];
+
       for (let i = 0; i < pokemons.length; i++) {
          const pokemon = pokemons[i];
-         pokemonList.innerHTML += convertPokemonToLi(pokemon);
-         
+         listItems.push(convertPokemonToLi(pokemon));
       }
-   })
-   .catch((error) => console.error(error))
+
+      console.log(listItems);
+})
+   
